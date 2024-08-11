bdd2ce1a2f265bdf73411732621da1cbf1e3b514
bekirasimcelik <bekirasimcelik@hotmail.com>
2024-08-11 22:11:05 +0300

    styles updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,40 +1,26 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { gradientTextColor } from "../constants/Colors";
 
 const Header: React.FC = () => {
   return (
     <header className="border-b-2 border-white shadow-md py-4 fixed top-0 w-full z-10">
       <div className="container mx-auto flex justify-between items-center px-4">
-        <Link
-          to="/"
-          className="text-3xl font-bold bg-gradient-to-b from-red-500 via-[gold] to-black text-transparent bg-clip-text"
-        >
+        <Link to="/" className={gradientTextColor}>
           MovieWebSite
         </Link>
         <nav className="hidden md:flex space-x-8">
-          <Link
-            to="/"
-            className="text-[gold] hover:text-transparent bg-clip-text bg-gradient-to-b from-red-500 via-[gold] to-black"
-          >
+          <Link to="/" className={gradientTextColor}>
             Films
           </Link>
-          <Link
-            to="/"
-            className="text-[gold] hover:text-transparent bg-clip-text bg-gradient-to-b from-red-500 via-[gold] to-black"
-          >
+          <Link to="/" className={gradientTextColor}>
             Series
           </Link>
-          <Link
-            to="/"
-            className="text-[gold] hover:text-transparent bg-clip-text bg-gradient-to-b from-red-500 via-[gold] to-black"
-          >
+          <Link to="/" className={gradientTextColor}>
             Actors
           </Link>
-          <Link
-            to="/favourites"
-            className="text-[gold] hover:text-transparent bg-clip-text bg-gradient-to-b from-red-500 via-[gold] to-black"
-          >
+          <Link to="/favourites" className={gradientTextColor}>
             Favourites
           </Link>
         </nav>
---
+++ src/constants/Colors.ts
@@ -0,0 +1,2 @@
+export const gradientTextColor =
+  "text-[gold] hover:text-transparent bg-clip-text bg-gradient-to-b from-red-500 via-[gold] to-black";
